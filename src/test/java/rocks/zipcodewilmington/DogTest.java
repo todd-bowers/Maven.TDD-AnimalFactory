247a19db80f18a43486e742a543dfe249c0bb814
todd-bowers <toddbows@gmail.com>
2022-07-02 21:15:12 -0400

    Dog setBirthDateTest

--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -2,7 +2,10 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
+
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
@@ -11,6 +14,19 @@ public class DogTest {
     // TODO - Create tests for `new Dog(String name, Date birthDate, Integer id)`
     // TODO - Create tests for `speak`
     // TODO - Create tests for `setBirthDate(Date birthDate)`
+    @Test
+    public void setBirthDateTest() {
+        // Given
+        Dog dog = new Dog(null, null, null);
+        Date givenBirthDate = new Date(1997-4-16);
+
+        // When
+        dog.setBirthDate(givenBirthDate);
+
+        // Then (we expect to get the given name from the cat)
+        Date dogBirthDate = dog.getBirthDate();
+        Assert.assertEquals(givenBirthDate, dogBirthDate);
+    }
     // TODO - Create tests for `void eat(Food food)`
     // TODO - Create tests for `Integer getId()`
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
